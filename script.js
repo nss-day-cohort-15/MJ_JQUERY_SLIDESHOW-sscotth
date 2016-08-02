a853da95ddbb4dc15427dd2569848a827ade76f6
Scott Humphries <git@sscotth.io>
2016-08-01 23:09:35 -0500

    initial ajax example

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 $(document).ready(function () {
+  var API_URL = 'http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC'
+
+  $.getJSON(API_URL, function (res) {
+    var images = res.data
+
+    // dynamically add the images to the page
+    images.forEach(function (img) {
+      $('.images-container').append(`<img src="${img.images.original.url}" />`)
+    })
+
+    $('img').first().addClass('active')
+  })
+
   $('.prev').click(function () {
     var activeImage = $('.active')
     var nextImage = activeImage.prev()
